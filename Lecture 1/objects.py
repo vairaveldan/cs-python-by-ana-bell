f129a172b122389736dc07e2743fbefb53bed389
vairaveldan <vair.71772114148@gct.ac.in>
2025-05-23 22:05:11 +0530

    objects.py added

---
+++ Lecture 1/objects.py
@@ -0,0 +1,13 @@
+# program manipulate objects
+# objects has a datatype
+# program do data-things using objects
+
+a = 1 # integer 
+b = 2.2 # float
+c = "hello" # string
+d = [1, 2, 3] # list
+e = {"name": "John", "age": 30} # dictionary
+f = (1, 2, 3) # tuple
+g = True # boolean
+h = None # NoneType
+i = 2+5j # complex
