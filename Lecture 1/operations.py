06c29a9e3c85ec354acfcddacbb1cdca4d11d8a7
vairaveldan <vair.71772114148@gct.ac.in>
2025-05-23 22:12:39 +0530

    operations.py added

---
+++ Lecture 1/operations.py
@@ -0,0 +1,24 @@
+x = 5
+y = 4
+
+# addition
+result = x + y
+print(result)  # Output: 9
+# subtraction
+result = x - y
+print(result)  # Output: 1
+# multiplication
+result = x * y
+print(result)  # Output: 20
+# division
+result = x / y
+print(result)  # Output: 1.25
+# modulus
+result = x % y
+print(result)  # Output: 1
+# exponentiation
+result = x ** y
+print(result)  # Output: 625
+# floor division
+result = x // y
+print(result)  # Output: 1
