3c3ba114bb8fd74b36ce5885d91ce828e9635ec5
vairaveldan <vair.71772114148@gct.ac.in>
2025-05-25 10:58:09 +0530

    string updated

--- Lecture 2/string.py
+++ Lecture 2/string.py
@@ -1,3 +1,13 @@
+# string arithmetic operations 
+str1 = "run"
+str2 =  "baby"
+# 1)concatenation 
+print (str1 + " " + str2)
+# 2)repetition
+print ((str1+' ')* 3)
+
+
+
 a="1234567890"
 print(a[10:-11:-1])
 print(a[-1:-11:-1])
