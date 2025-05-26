4a9569dd9ab0ec51a4a04cf9f52fbc2a7af8c45f
vairaveldan <vair.71772114148@gct.ac.in>
2025-05-26 19:27:00 +0530

    f-print.py updated

--- Lecture 2/f-print.py
+++ Lecture 2/f-print.py
@@ -1,3 +1,7 @@
 Percentage = 83.3
 Name = "vairavel"
-print(f"this is {Name} form GCT-ECE'25 and I got {Percentage}% score in my B.E Degree.")
+print(f"this is {Name} from GCT-ECE'25 and I got {Percentage}% score in my B.E Degree.")
+
+
+# alternative way to print the string
+print ("this is",Name,"from GCT-ECE'25 and I got",Percentage,"% score in my B.E Degree.")
