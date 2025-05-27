4e7ce4c55099c40930e48382072f8abcf6b0ee68
vairaveldan <vair.71772114148@gct.ac.in>
2025-05-27 21:12:24 +0530

    woods game added

---
+++ Lecture 3/2D_woods_game.py
@@ -0,0 +1,16 @@
+# initialize the backgrounds 
+wood_background = "Woods"
+exit_background = "Exit"
+background = wood_background
+
+while (background == wood_background) : 
+    background = input ("Enter the direction to go(Right or Left):").strip()
+    if background.capitalize() == "Right" :
+        background = wood_background
+        print("trapped in the woods")
+    elif background.capitalize() == "Left":
+        background = exit_background
+    else :
+        print("Invalid direction. Please enter Right or Left.")
+        background = wood_background
+print("you successfully escaped from the woods")
